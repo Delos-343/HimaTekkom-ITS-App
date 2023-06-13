45d8986f7f4efa9ae2a06953bceee21ea46636e1
admiralkuznetsov45 <andafsul4@gmail.com>
2023-06-13 15:28:01 +0700

    fixing bug

--- AppHimaTekkomITS-master/components/Screen2.js
+++ AppHimaTekkomITS-master/components/Screen2.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import newsData from '../data/newsData.json';
-import '../data/hyGraph'
 
 const Screen2 = () => {
   const navigation = useNavigation();
 
-  const renderNewsItem = ({ deskripsi, title, konten, image }) => {
+  const renderNewsItem = ({ item }) => {
     return (
       <TouchableOpacity onPress={() => navigation.navigate('NewsItem', { item })}>
         <View style={styles.container}>
