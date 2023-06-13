818197c1b4b56ca04c27f7908482460cb70d9d55
admiralkuznetsov45 <andafsul4@gmail.com>
2023-06-14 01:07:59 +0700

    wub

--- AppHimaTekkomITS-master/App.js
+++ AppHimaTekkomITS-master/App.js
@@ -7,7 +7,7 @@ import Screen1 from './components/Screen1';
 import Screen2 from './components/Screen2';
 import Screen3 from './components/Screen3';
 import NewsItem from './components/ScreenNews';
-import { TestScreen } from './components/TestScreen';
+import TestScreen from './components/TestScreen';
 
 const Stack = createStackNavigator();
 
