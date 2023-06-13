42e78b6b9f24dc0be2b404c98f4963e05e858168
admiralkuznetsov45 <andafsul4@gmail.com>
2023-06-14 01:20:11 +0700

    give up

--- AppHimaTekkomITS-master/data/hyGraph.js
+++ AppHimaTekkomITS-master/data/hyGraph.js
@@ -5,7 +5,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
   const query = gql`
-    query Authors {
+    query MyQuery {
         postsConnection {
         edges {
             node {
