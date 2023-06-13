8519089c4033e10d87050369cfb42178ab611a6d
admiralkuznetsov45 <andafsul4@gmail.com>
2023-06-13 15:32:53 +0700

    added new async await

--- AppHimaTekkomITS-master/data/hyGraph.js
+++ AppHimaTekkomITS-master/data/hyGraph.js
@@ -65,4 +65,41 @@ export const getPosts = async () => {
     const result = await request(graphqlAPI, query);
   
     return result.postsConnection.edges;
-  };  
+  };
+
+  export const getPosts = async () => {
+    const query = gql`
+      query MyQuery {
+        postsConnection {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query);
+  
+    return result.postsConnection.edges;
+  };
