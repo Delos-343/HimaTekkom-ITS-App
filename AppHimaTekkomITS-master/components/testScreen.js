c9d364a857345fedfc73c65476c63a015bc3ad78
admiralkuznetsov45 <andafsul4@gmail.com>
2023-06-14 19:24:03 +0700

    GET Apollo

--- AppHimaTekkomITS-master/components/testScreen.js
+++ AppHimaTekkomITS-master/components/testScreen.js
@@ -2,46 +2,64 @@ import React from 'react';
 import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import moment from 'moment';
-import { getPosts } from '../data/hyGraph';
+import { getPostsQuery } from '../data/hyGraph';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
 const TestScreen = () => {
 
   const navigation = useNavigation();
 
-  const renderNewsItem = ({ posts, post }) => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const client = new ApolloClient({
+      // Your API endpoint here
+    });
+
+    const { data } = await client.query(getPostsQuery);
+
+    setPosts(data.posts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderNewsItem = ({ post }) => {
 
     console.log(post);
 
     return (
       <TouchableOpacity onPress={() => navigation.navigate('NewsItem', { post })}>
-        {posts.map((post) =>
-          <View style={styles.container}>
-            <Image
-              source={{ uri: post.featuredImage.url }}
-              style={{ width: 100, height: 100, marginRight: 10 }}
-            />
-            <View style={styles.newsreel}>
-              <Text style={styles.title}>
-                {post.title}
-              </Text>
-              <Text style={styles.date}>
-                {moment(post.createdAt).format("MM/DD/YYYY")}
-              </Text>
-              <Text numberOfLines={3}>{post.content}</Text>
-            </View>
+        <View style={styles.container}>
+          <Image
+            source={{ uri: post.featuredImage.url }}
+            style={{ width: 100, height: 100, marginRight: 10 }}
+          />
+          <View style={styles.newsreel}>
+            <Text style={styles.title}>
+              {post.title}
+            </Text>
+            <Text style={styles.date}>
+              {moment(post.createdAt).format("MM/DD/YYYY")}
+            </Text>
+            <Text numberOfLines={3}>{post.content}</Text>
           </View>
-        )}
+        </View>
       </TouchableOpacity>
     );
   };
 
   return (
-    <FlatList
-      data={getPosts}
-      renderItem={renderNewsItem}
-      keyExtractor={(post) => post.id.toString()}
-      style={{ padding: 10 }}
-    />
+    <ApolloProvider client={client}>
+      <FlatList
+        data={posts}
+        renderItem={renderNewsItem}
+        keyExtractor={(post) => post.id.toString()}
+        style={{ padding: 10 }}
+      />
+    </ApolloProvider>
   );
 };
 
