3b7296fab5b3e7eac4933f9651c2c27a8db8e474
admiralkuznetsov45 <andafsul4@gmail.com>
2023-06-15 19:14:13 +0700

    fixed Stacks

--- AppHimaTekkomITS-master/App.js
+++ AppHimaTekkomITS-master/App.js
@@ -9,17 +9,19 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator
-            initialRouteName="SplashScreen"
-            screenOptions={{ headerShown: false }}
-        >
+        <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
+
             <Stack.Screen name="SplashScreen" component={SplashScreen} />
             <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            
             <Stack.Screen name="Screen1" component={Screen1} />
             <Stack.Screen name="Screen2" component={Screen2}  options={{ title: 'Berita' }} />
             <Stack.Screen name="Screen3" component={Screen3}  />
+
             <Stack.Screen name="NewsItem" component={NewsItem} options={{ title: 'Detail Berita' }} />
+
             <Stack.Screen name="TestScreen" component={TestScreen} options={{ title: 'Testing Environment' }} />
+
         </Stack.Navigator>
     </NavigationContainer>
   );
