dd9ab72c81859f114f4688a4367974a0501a493e
admiralkuznetsov45 <andafsul4@gmail.com>
2023-06-15 19:13:10 +0700

    clearing out a few shit

--- AppHimaTekkomITS-master/App.js
+++ AppHimaTekkomITS-master/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { ApolloProvider } from '@apollo/client';
-import client from './utils/ApolloClient';
-import MyStackNavigator from './stacks/AppStackNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen, Screen1, Screen2, Screen3, SplashScreen, TestScreen } from './components';
+import NewsItem from './components/ScreenNews';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <ApolloProvider client={client}>
-      <MyStackNavigator />
-    </ApolloProvider>
+    <NavigationContainer>
+        <Stack.Navigator
+            initialRouteName="SplashScreen"
+            screenOptions={{ headerShown: false }}
+        >
+            <Stack.Screen name="SplashScreen" component={SplashScreen} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="Screen1" component={Screen1} />
+            <Stack.Screen name="Screen2" component={Screen2}  options={{ title: 'Berita' }} />
+            <Stack.Screen name="Screen3" component={Screen3}  />
+            <Stack.Screen name="NewsItem" component={NewsItem} options={{ title: 'Detail Berita' }} />
+            <Stack.Screen name="TestScreen" component={TestScreen} options={{ title: 'Testing Environment' }} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
