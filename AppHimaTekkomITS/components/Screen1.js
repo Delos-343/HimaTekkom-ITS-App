50ff77d7db314ec31b351a8e2e02b37e525788bb
Delos-343 <mfachry.handoko@gmail.com>
2023-06-24 16:38:21 +0700

    fixed styling and added new features

--- AppHimaTekkomITS/components/Screen1.js
+++ AppHimaTekkomITS/components/Screen1.js
@@ -7,18 +7,21 @@ export default function Screen1() {
   return (
     <View style={styles.container}>
       <View style={styles.frameContainer}>
-
-        <VideoDisplay />
-
         <Text style={styles.title}>
           LIVE NOW
         </Text>
+        
+        <VideoDisplay />
+
+        <View style={styles.divider}></View>
+
         <Text style={styles.bodyText}>
           HIMATEKKOM ITS dilatarbelakangi
           oleh kondisi mahasiswa teknik komputer ITS yang
           masih dinaungi oleh himpunan induk, HIMATEKTRO
           ITS.
-          
+        </Text>  
+        <Text style={styles.bodyText}>  
           Adapun keresahan yang dimiliki oleh 
           mahasiswa Teknik Komputer ITS antara lain isu
           keprofesian, pengembangan mahasiswa, dan 
@@ -36,7 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
-    marginTop: Dimensions.get('window').height * 0.4,
   },
   logo: {
     width: '50%',
@@ -46,13 +48,12 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 40,
     fontWeight: 'bold',
+    margin: 'auto',
+    paddingBottom: 50,
+    paddingHorizontal: 10,
   },
-  square: {
-    width: Dimensions.get('window').width * 0.8,
-    height: Dimensions.get('window').width * 0.8,
-    borderWidth: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
+  divider:{
+    marginTop: 50,
   },
   bodyText: {
     color: 'black',
